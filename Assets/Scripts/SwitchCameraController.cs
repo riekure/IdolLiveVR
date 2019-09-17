9921b1fc3cbfbbf685db3ec9f47d665fc407b5f8
riekure <s2darl0uvgia24@gmail.com>
2019-09-17 17:07:22 +0900

    create switchCameraController

---
+++ Assets/Scripts/SwitchCameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCameraController : MonoBehaviour 
+{
+    public GameObject MainCamera;
+    public GameObject DiveCamera;
+
+    public void OnClickMainCameraButton()
+    {
+        MainCamera.SetActive(false);
+        DiveCamera.SetActive(true);
+    }
+
+    public void OnClickDiveCameraButton()
+    {
+        MainCamera.SetActive(true);
+        DiveCamera.SetActive(false);
+    }
+}
